4990361ae7bb5666dab5d57ff4fc65ef0c43ceb8
Omar Fathi22 <ommarf525@gmail.com>
2024-04-03 15:15:56 -0700

    102-magic_calculation.py: 103-python.c

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    result = 0
+    i = 1
+    while i < 3:
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result += a ** b / i
+        except Exception:
+            result = b + a
+            break
+        i += 1
+    return result
