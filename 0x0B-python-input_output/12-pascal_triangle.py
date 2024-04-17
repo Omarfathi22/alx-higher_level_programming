89e7a96f13b3ac5a85acd71892e4391a9fc2086c
Omar Fathi22 <ommarf525@gmail.com>
2024-04-16 21:10:06 -0700

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # Initialize with the first row
+    for i in range(1, n):
+        row = [1]  # Start each row with 1
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])  # Calculate the value based on the previous row
+        row.append(1)  # End each row with 1
+        triangle.append(row)  # Add the row to the triangle
+    return triangle
