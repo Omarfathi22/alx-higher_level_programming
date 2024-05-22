4fad129cf113af74eed18efeebb168d823a244bf
Omar Fathi22 <ommarf525@gmail.com>
2024-05-22 12:22:37 -0700

    0:3

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Create the MySQL server user user_0d_1 with all privileges
+
+-- Create user_0d_1 if not exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
