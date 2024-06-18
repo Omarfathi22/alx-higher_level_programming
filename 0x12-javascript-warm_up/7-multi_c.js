7de26bc3014c0c0a73a0e09c3025a669a0985b75
Omar Fathi22 <ommarf525@gmail.com>
2024-06-18 14:46:58 -0700

    ./7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const numArg = parseInt(process.argv[2]);
+
+if (isNaN(numArg) || numArg <= 0) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < numArg; i++) {
+    console.log('C is fun');
+  }
+}
