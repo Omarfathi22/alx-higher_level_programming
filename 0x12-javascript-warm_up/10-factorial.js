2e68c24a81226136b8aec7932b18fa74029dac07
Omar Fathi22 <ommarf525@gmail.com>
2024-06-20 16:45:34 -0700

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
-function factorial(n) {
-  if (isNaN(n)) {
+const args = process.argv;
+const n = parseInt(args[2]);
+
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
     return 1;
   }
-
-  if (n === 0 || n === 1) {
+  if (n === 1) {
     return 1;
   }
-
   return n * factorial(n - 1);
 }
 
-const arg = parseInt(process.argv[2]);
-console.log(factorial(arg));
+console.log(factorial(n));
