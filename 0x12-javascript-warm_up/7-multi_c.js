ab488424c01700a839cad1340a76c03cada5323b
Omar Fathi22 <ommarf525@gmail.com>
2024-06-20 16:24:54 -0700

    fixed

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
-const numArg = parseInt(process.argv[2]);
 
-if (isNaN(numArg) || numArg <= 0) {
+const args = process.argv;
+const x = parseInt(args[2]);
+
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < numArg; i++) {
+  let i = 0;
+  while (i < x) {
     console.log('C is fun');
+    i++;
   }
 }
