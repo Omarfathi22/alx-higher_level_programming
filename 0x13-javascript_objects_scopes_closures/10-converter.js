dd3a1adb9118e82133efa682553b25e925203223
Omar Fathi22 <ommarf525@gmail.com>
2024-06-20 18:05:28 -0700

    10-error

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-
-
-exports.converter = function(base) {
-  return function convert(number) {
-    if (number === 0) {
-      return '';
-    } else {
-      const remainder = number % base;
-      return convert(Math.floor(number / base)) + (remainder < 10 ? remainder : String.fromCharCode(remainder + 87));
-    }
+exports.converter = function (base) {
+  return function (number) {
+    return number.toString(base);
   };
 };
