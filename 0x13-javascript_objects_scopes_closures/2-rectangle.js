dd52d9e225a8a64ee86de7aab979680b60d5b4bd
Omar Fathi22 <ommarf525@gmail.com>
2024-06-20 18:17:35 -0700

    2-fix error

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,36 @@
 #!/usr/bin/node
+
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // If w or h is not positive, create an empty object
       return {};
     }
-    this.width = w;
-    this.height = h;
   }
 }
 
 module.exports = Rectangle;
+
+// Test code
+const r1 = new Rectangle(2, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
+
+const r2 = new Rectangle(2, -3);
+console.log(r2);
+console.log(r2.width);
+console.log(r2.height);
+
+const r3 = new Rectangle(2);
+console.log(r3);
+console.log(r3.width);
+console.log(r3.height);
+
+const r4 = new Rectangle(2, 0);
+console.log(r4);
+console.log(r4.width);
+console.log(r4.height);
