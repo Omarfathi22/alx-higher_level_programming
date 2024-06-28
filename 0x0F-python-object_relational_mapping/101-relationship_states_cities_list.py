9ec974da109354bf1d71ce58fd202d07569af52b
Omar Fathi22 <ommarf525@gmail.com>
2024-06-28 23:14:18 +0300

    fix-101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,21 +1,36 @@
 #!/usr/bin/python3
-""" prints the State object with THE name passed as argument from the database
 """
+Prints all State objects along with their related City objects from the database.
+
+This script connects to a MySQL database using SQLAlchemy, retrieves all State 
+objects, and for each State, it retrieves and prints the associated City objects.
+"""
+
 import sys
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import relationship
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # Create a new SQLAlchemy engine instance, connecting to the MySQL database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    
+    # Create all tables in the database (based on the Base class metadata)
     Base.metadata.create_all(engine)
+
+    # Create a configured "Session" class and an instance of a session
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    # Query all State objects and order them by their id
     for instance in session.query(State).order_by(State.id):
+        # Print the State id and name
         print(instance.id, instance.name, sep=": ")
+
+        # For each State, query and print the associated City objects
         for city_ins in instance.cities:
             print("    ", end="")
             print(city_ins.id, city_ins.name, sep=": ")
+
